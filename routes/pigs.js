6c945354e0cfeda0e1352456d12887b24e7a6b27
Lux Protoss <zerox12311@gmail.com>
2018-10-25 15:29:54 +0800

    Add Hyperledger client code

--- routes/pigs.js
+++ routes/pigs.js
@@ -72,13 +72,83 @@ router.post('/addAction', (req, res, next) => {
             res.status(401).end();
         }
         console.log(decoded);
+        const pigId = req.body.pigId;
+        if (!pigId) {
+            res.status(400).send('Please input pig id');
+        }
+        const actionName = req.body.actionName;
+        if (!actionName) {
+            res.status(400).send('Please input action name');
+        }
+        const company = decoded.company;
+        const date = new Date().toISOString().slice(0, 10);
         // call hyperledger api
+
+
+        // call hyperledger api
+        Fabric_Client.newDefaultKeyValueStore({
+            path: store_path
+        }).then((state_store) => {
+            // assign the store to the fabric client
+            fabric_client.setStateStore(state_store);
+            var crypto_suite = Fabric_Client.newCryptoSuite();
+            // use the same location for the state store (where the users' certificate are kept)
+            // and the crypto store (where the users' keys are kept)
+            var crypto_store = Fabric_Client.newCryptoKeyStore({ path: store_path });
+            crypto_suite.setCryptoKeyStore(crypto_store);
+            fabric_client.setCryptoSuite(crypto_suite);
+
+            // get the enrolled user from persistence, this user will sign all requests
+            return fabric_client.getUserContext('user1', true);
+        }).then((user_from_store) => {
+            if (user_from_store && user_from_store.isEnrolled()) {
+                console.log('Successfully loaded user1 from persistence');
+                member_user = user_from_store;
+            } else {
+                throw new Error('Failed to get user1.... run registerUser.js');
+            }
+
+            // queryCar chaincode function - requires 1 argument, ex: args: ['CAR4'],
+            // queryAllCars chaincode function - requires no arguments , ex: args: [''],
+            const request = {
+                //targets : --- letting this default to the peers assigned to the channel
+                chaincodeId: 'pigchain',
+                fcn: 'recordPig',
+                args: [pigId, company, actionName, date]
+            };
+
+            // send the query proposal to the peer
+            return channel.queryByChaincode(request);
+        }).then((query_responses) => {
+            console.log("Query has completed, checking results");
+            // query_responses could have more than one  results if there multiple peers were used as targets
+            if (query_responses && query_responses.length == 1) {
+                if (query_responses[0] instanceof Error) {
+                    console.error("error from query = ", query_responses[0]);
+                    res.status(400).send(query_responses[0]);
+                } else {
+                    console.log("Response is ", query_responses[0].toString());
+                    res.send(query_responses[0].toString());
+                }
+            } else {
+                res.send("No payloads were returned from query")
+                console.log("No payloads were returned from query");
+            }
+        }).catch((err) => {
+            res.status(400).send(err);
+            console.error('Failed to query successfully :: ' + err);
+        });
 
         res.send(decoded);
     });
 })
 
 router.get('/queryPig', async (req, res, next) => {
+    const pigId = req.body.pigId;
+    if (!pigId) {
+        res.status(400).send('Please input pig id');
+    }
+
     // call hyperledger api
     Fabric_Client.newDefaultKeyValueStore({
         path: store_path
@@ -106,9 +176,9 @@ router.get('/queryPig', async (req, res, next) => {
         // queryAllCars chaincode function - requires no arguments , ex: args: [''],
         const request = {
             //targets : --- letting this default to the peers assigned to the channel
-            chaincodeId: 'fabcar',
-            fcn: 'queryAllTuna',
-            args: []
+            chaincodeId: 'pigchain',
+            fcn: 'queryPigHistory',
+            args: [pigId]
         };
 
         // send the query proposal to the peer
